76f937e0a89f4973700a9a9ce8250ebc9adfaed5
Tao-xu <taoxu@163.com>
2016-12-10 14:47:31 +0800

    install docker-compose in yum

---
+++ install-docker-compose-yum.sh
@@ -0,0 +1,6 @@
+echo "install wget"
+yum install wget
+echo "install pip"
+wget https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py # python get-pip.py
+echo "install docker compose"
+pip install docker-compose
